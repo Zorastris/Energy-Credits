a3e80800bd3aa6414b603e6c1c30a8608501397b
Batu Büktel <batubuktel@gmail.com>
2021-08-03 00:27:11 +0300

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,7 @@
 const Migrations = artifacts.require("Migrations");
-const EnergyCredits = artifacts.require("EnergyCredits");
-const EnergyMarket = artifacts.require("EnergyMarket");
+
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(EnergyCredits);
-  deployer.deploy(EnergyMarket);
+
 };
