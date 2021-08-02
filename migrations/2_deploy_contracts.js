e80abd16cd38b65d39cfeb3ace37aca26474a643
Batu Büktel <batubuktel@gmail.com>
2021-08-03 00:27:07 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const EnergyCredits = artifacts.require("EnergyCredits");
 const EnergyMarket = artifacts.require("EnergyMarket");
 
-module.exports = function(deployer) {
-  await deployer.deploy(EnergyCredits);
-  deployer.deploy(EnergyCredits);
-  const credits = await EnergyCredits.deployed();
-  await deployer.deploy(EnergyMarket, credits.address);
+module.exports =  function(deployer) {
+  deployer.deploy(EnergyCredits)
+        // Wait until the storage contract is deployed
+        .then(() => EnergyCredits.deployed())
+        // Deploy the InfoManager contract, while passing the address of the
+        // Storage contract
+        .then(() => deployer.deploy(EnergyMarket, EnergyCredits.address));
 };
